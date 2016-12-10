c4853e78ef206da9680ee44fc171efea8f258ddc
Jose Javier Escudero Gómez <doublejay93@gmail.com>
2016-12-10 13:28:03 +0100

    Add new println
    Add new line to test GitHubb Desktop Application

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hi, this is my Hello World aplication for learn to use Github.");
+		System.out.println("Now I'm testing the GitHub Desktop Application.");
 	}
 
 }
